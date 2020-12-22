410c9444e4a212e904bca603181919056c6b3632
ndinardo <nicola.dinardo@ticketeasy.it>
2020-12-22 12:54:24 +0100

    adding remote package

--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello, world!", "Hello, Go!"))
 }
